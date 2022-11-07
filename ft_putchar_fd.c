adb325491cb208c69807f33ba21e03749e6bd544
mbarberi <mbarberi@student.42lyon.fr>
2022-11-07 14:02:15 +0100

    Added header.

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbarberi <mbarberi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/07 14:00:26 by mbarberi          #+#    #+#             */
+/*   Updated: 2022/11/07 14:00:27 by mbarberi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 /*
@@ -5,8 +17,7 @@
 ** corresponding file descriptor. For example, 0 is the stdin, 1 is
 ** the stdout and 2 is stderr.
 */
-
-int ft_putchar_fd(int fd, int c);
+int ft_putchar_fd(int fd, int c)
 {
   if (write(fd, &c, 1))
     return (c);

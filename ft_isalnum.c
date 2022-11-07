42ac4d3391d023bb238f18967f32aa990a054d68
mbarberi <mbarberi@student.42lyon.fr>
2022-11-07 15:35:24 +0100

    Fixed norm problems.

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,17 +6,18 @@
 /*   By: mbarberi <mbarberi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 13:54:14 by mbarberi          #+#    #+#             */
-/*   Updated: 2022/11/07 13:54:19 by mbarberi         ###   ########.fr       */
+/*   Updated: 2022/11/07 15:19:46 by mbarberi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-** The isalnum() function tests for any character for which isalpha(3)
+** The isalnum(3) function tests for any character for which isalpha(3)
 ** or isdigit(3) is true.
 */
-int ft_isalnum(int c)
+int	ft_isalnum(int c)
 {
-  return (c >= '0' && c <= '9' || c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z');
+	return (c >= '0' && c <= '9' || c >= 'a'
+		&& c <= 'z' || c >= 'A' && c <= 'Z');
 }

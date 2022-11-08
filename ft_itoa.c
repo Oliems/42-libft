2db2e6ddf813981bb7b808d8de0a83ed8118d3a2
Oliems <therealoliems@gmail.com>
2022-11-08 14:15:21 +0100

    Added function definition and description.

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,16 @@
 /*   By: mbarberi <mbarberi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 13:56:32 by mbarberi          #+#    #+#             */
-/*   Updated: 2022/11/07 13:56:33 by mbarberi         ###   ########.fr       */
+/*   Updated: 2022/11/08 14:07:45 by mbarberi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+** Alloue (avec malloc(3)) et retourne une chaîne
+** de caractères représentant l’entier ’n’ reçu en
+** argument. Les nombres négatifs doivent être gérés.
+*/
+char	*ft_itoa(int n)
+{
+
+}

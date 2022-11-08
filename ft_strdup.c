8b8f1410f33a1a44dd0563e6a394e694ba9949c2
Oliems <therealoliems@gmail.com>
2022-11-08 14:10:32 +0100

    Fixed function definition according to prorotype.

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: mbarberi <mbarberi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 14:01:09 by mbarberi          #+#    #+#             */
-/*   Updated: 2022/11/07 14:01:10 by mbarberi         ###   ########.fr       */
+/*   Updated: 2022/11/08 14:09:46 by mbarberi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 /*
 ** The strdup() function allocates sufficient memory for a copy of
-** the string s1, does the copy, and returns a pointer to it. The
+** the string str, does the copy, and returns a pointer to it. The
 ** pointer may subsequently be used as an argument to the function
 ** free(3).
 */
-char *ft_strdup(const char *s1)
+char	*ft_strdup(const char *str)
 {
   char *s2;
 
-  s2 = (char *) ft_strlen(s1) + 1;
+  s2 = (char *) ft_strlen(str) + 1;
   if (s2 != NULL)
-    ft_strlcpy(s2, s1, sizeof(s2));
+    ft_strlcpy(s2, str, sizeof(s2));
   return (s2);
 }

d52bdc5ed7f66a631dcef4c35b5bebfb7f422a91
Oliems <therealoliems@gmail.com>
2022-11-08 13:14:04 +0100

    Implemented ft_calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,31 @@
 /*   By: mbarberi <mbarberi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 13:54:06 by mbarberi          #+#    #+#             */
-/*   Updated: 2022/11/07 13:54:19 by mbarberi         ###   ########.fr       */
+/*   Updated: 2022/11/08 13:13:08 by mbarberi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+/*
+** The calloc() function allocates space for /number/ objects, each /size/ bytes
+** in length.  The result is identical to calling malloc() with an argument
+** of ``number * size'', with the exception that the allocated memory is
+** explicitly initialized to zero bytes.
+*/
+void	*ft_calloc(size_t number, size_t size)
+{
+	size_t	*p;
+	size_t	*pp;
+
+	p = malloc(number * size);
+	if (!p)
+		return (NULL);
+	pp = p;
+	while ((unsigned long)(pp - p) != (number * size))
+	{
+		pp++;
+		*pp = 0;
+	}
+	return ((void *)(p));
+}

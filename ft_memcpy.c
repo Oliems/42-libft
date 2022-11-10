d567d5d45ef39cf9fb15c5d2d3a136f7137a1cca
Oliems <therealoliems@gmail.com>
2022-11-10 13:07:37 +0100

    Implemented ft_memcpy.

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,27 @@
 /*   By: mbarberi <mbarberi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 13:57:14 by mbarberi          #+#    #+#             */
-/*   Updated: 2022/11/07 13:57:15 by mbarberi         ###   ########.fr       */
+/*   Updated: 2022/11/10 13:07:02 by mbarberi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+/*
+** The memcpy() function copies len bytes from string src to string dst.
+** The arguments must not overlap -- behavior if the arguments overlap is
+** undefined.
+*/
+void	*ft_memcpy(void *dst, const void *src, size_t len)
+{
+	void		*d;
+	const void	*s;
+
+	d = dst;
+	s = src;
+	if (!len)
+		return (dst);
+	while (len--)
+		*(unsigned char *)d++ = *(unsigned char *)s++;
+	return (dst);
+}

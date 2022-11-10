17d5970fa4a4f0aa4607942f8003d397f0561663
Oliems <therealoliems@gmail.com>
2022-11-10 15:00:47 +0100

    Implemented memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,26 @@
 /*   By: mbarberi <mbarberi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 13:57:06 by mbarberi          #+#    #+#             */
-/*   Updated: 2022/11/07 13:57:06 by mbarberi         ###   ########.fr       */
+/*   Updated: 2022/11/10 15:00:24 by mbarberi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+/*
+** The memchr() function locates the first occurrence of c (converted to an
+** unsigned char) in string b.
+*/
+void	*ft_memchr(const void *b, int c, size_t len)
+{
+	unsigned char	*p;
+
+	p = (unsigned char *)b;
+	while (len--)
+	{
+		if (*p == c)
+			return ((void *)p);
+		p++;
+	}
+	return (NULL);
+}

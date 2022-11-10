beafe04b5bd23313bfd56e66ed59b95f22f2814e
Oliems <therealoliems@gmail.com>
2022-11-10 15:57:52 +0100

    Implemented strrchr.

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,26 @@
 /*   By: mbarberi <mbarberi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 14:01:40 by mbarberi          #+#    #+#             */
-/*   Updated: 2022/11/07 14:01:41 by mbarberi         ###   ########.fr       */
+/*   Updated: 2022/11/10 15:57:29 by mbarberi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+/*
+** The strrchr() function locates the last occurrence of c (converted to a
+** char) in the string s.  If c is `\0', strrchr() locates the terminating
+** `\0'.
+*/
+char	*ft_strrchr(const char *s, int c)
+{
+	char	*p;
+
+	p = (char *)s + ft_strlen(s) + 1;
+	while (*p != (char)c)
+	{
+		if (p-- <= s)
+			return (NULL);
+	}
+	return (p);
+}

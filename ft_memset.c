7bc45906bdc8e8a7a11595e1d7c5de99e823728f
Oliems <therealoliems@gmail.com>
2022-11-10 12:56:52 +0100

    Implemented ft_memset.

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,24 @@
 /*   By: mbarberi <mbarberi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 13:57:20 by mbarberi          #+#    #+#             */
-/*   Updated: 2022/11/07 13:57:20 by mbarberi         ###   ########.fr       */
+/*   Updated: 2022/11/10 12:56:36 by mbarberi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+/*
+** The memset() function writes len bytes of value c (converted to an
+** unsigned char) to the string b.
+*/
+void	*ft_memset(void *b, int c, size_t len)
+{
+	void *p;
+
+	p = b;
+	if (len == 0)
+		return (b);
+	while (len--)
+		*(unsigned char *)p++ = (unsigned char)c;
+	return (b);
+}

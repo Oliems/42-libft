8c36383d3eb211c13f9eacf0a8f5a44c5b0eb988
Oliems <therealoliems@gmail.com>
2022-11-10 15:34:23 +0100

    Implemented strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,34 @@
 /*   By: mbarberi <mbarberi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 14:01:04 by mbarberi          #+#    #+#             */
-/*   Updated: 2022/11/07 14:01:06 by mbarberi         ###   ########.fr       */
+/*   Updated: 2022/11/10 15:25:45 by mbarberi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+/*
+** The strchr() and strchrnul() functions locate the first occurrence of c
+** in the string pointed to by s.  The terminating NUL character is consid-
+** ered part of the string.  If c is `\0', strchr() and strchrnul() locate
+** the terminating `\0'.
+*/
+char	*ft_strchr(const char *s, int c)
+{
+	char	*p;
+
+	p = (char *)s;
+	while (*p != (char)c)
+	{
+		if (!*p++)
+			return (NULL);
+	}
+	return (p);
+}
+
+int main(void)
+{
+	char *s = "Hello";
+	ft_strchr(s, 'a');
+	return 0;
+}

75daaeed389e7a7ca4cf63a65c5e57a595ce9d2f
Oliems <therealoliems@gmail.com>
2022-11-10 10:25:29 +0100

    Fixed Norme and compilation errors.

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,19 +6,19 @@
 /*   By: mbarberi <mbarberi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 14:01:25 by mbarberi          #+#    #+#             */
-/*   Updated: 2022/11/07 14:01:26 by mbarberi         ###   ########.fr       */
+/*   Updated: 2022/11/10 10:24:52 by mbarberi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /* The strlen() function computes the length of the string s. */
-size_t strlen(const char *s)
+size_t	ft_strlen(const char *s)
 {
-  char *p;
+	const char	*p;
 
-  p = s;
-  while (*p)
-    p++;
-  return ((size_t)(p - s));
+	p = s;
+	while (*p)
+		p++;
+	return ((size_t)(p - s));
 }

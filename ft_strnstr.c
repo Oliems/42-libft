6988237ce9a413135d56fd85f6971bf375f8b0dd
Oliems <therealoliems@gmail.com>
2022-11-11 11:42:01 +0100

    Implemented ft_strnstr and unit test.

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,41 @@
 /*   By: mbarberi <mbarberi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 14:01:36 by mbarberi          #+#    #+#             */
-/*   Updated: 2022/11/07 14:01:37 by mbarberi         ###   ########.fr       */
+/*   Updated: 2022/11/11 11:37:39 by mbarberi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+/*
+** The strnstr() function locates the first occurrence of the
+** NUL-terminated string little in the string big, where not more than len
+** characters are searched.
+*/
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	const char	*b;
+	const char	*l;
+	const char	*head;
+
+	if (!*little)
+		return ((char *)big);
+	if (!*big || !len)
+		return (NULL);
+	head = big;
+	while (*head && len--)
+	{
+		b = head;
+		l = little;
+		while (*b && *l && (*b == *l) && len)
+		{
+			b++;
+			l++;
+			len--;
+		}
+		if (*l == '\0')
+			return ((char *)(head));
+		head++;
+	}
+	return (NULL);
+}
--- test/suite.c
+++ test/suite.c
@@ -7,47 +7,81 @@
 #include <strings.h>
 
 /* CTYPE */
-MU_TEST(test_isalpha) {
+MU_TEST(test_isalpha)
+{
 	for (int i = 0; i <= 127; i++)
 		mu_check(ft_isalpha(i) == isalpha(i));
 }
-MU_TEST(test_isdigit) {
+MU_TEST(test_isdigit)
+{
 	for (int i = 0; i <= 127; i++)
 		mu_check(ft_isdigit(i) == isdigit(i));
 }
-MU_TEST(test_isalnum) {
+MU_TEST(test_isalnum)
+{
 	for (int i = 0; i <= 127; i++)
 		mu_check(ft_isalnum(i) == isalnum(i));
 }
-MU_TEST(test_isascii) {
+MU_TEST(test_isascii)
+{
 	for (int i = 0; i <= 127; i++)
 		mu_check(ft_isascii(i) == isascii(i));
 }
-MU_TEST(test_isprint) {
+MU_TEST(test_isprint)
+{
 	for (int i = 0; i <= 127; i++)
 		mu_check(ft_isprint(i) == isprint(i));
 }
-MU_TEST(test_toupper) {
+MU_TEST(test_toupper)
+{
 	for (int i = 0; i <= 127; i++)
 		mu_check(ft_toupper(i) == toupper(i));
 }
-MU_TEST(test_tolower) {
+MU_TEST(test_tolower)
+{
 	for (int i = 0; i <= 127; i++)
 		mu_check(ft_tolower(i) == tolower(i));
 }
 
-MU_TEST_SUITE(test_ctype) {
-  MU_RUN_TEST(test_isalpha);
-  MU_RUN_TEST(test_isdigit);
-  MU_RUN_TEST(test_isalnum);
-  MU_RUN_TEST(test_isascii);
-  MU_RUN_TEST(test_isprint);
-  MU_RUN_TEST(test_toupper);
-  MU_RUN_TEST(test_tolower);
+MU_TEST_SUITE(test_ctype)
+{
+	MU_RUN_TEST(test_isalpha);
+	MU_RUN_TEST(test_isdigit);
+	MU_RUN_TEST(test_isalnum);
+	MU_RUN_TEST(test_isascii);
+	MU_RUN_TEST(test_isprint);
+	MU_RUN_TEST(test_toupper);
+	MU_RUN_TEST(test_tolower);
 }
 
-int main(void) {
-  MU_RUN_SUITE(test_ctype);
-  MU_REPORT();
-  return MU_EXIT_CODE;
+/* STRING */
+MU_TEST(test_strnstr)
+{
+	char *ori;
+	char *cpy;
+	ori = strnstr("Hello World!", "llo", 4);
+	cpy = ft_strnstr("Hello World!", "llo", 4);
+	mu_assert_string_eq(ori, cpy);
+	ori = strnstr("Hello World!", "llo", 0);
+	cpy = ft_strnstr("Hello World!", "llo", 0);
+	mu_assert_string_eq(ori, cpy);
+	ori = strnstr("Hello World!", "", 4);
+	cpy = ft_strnstr("Hello World!", "", 4);
+	mu_assert_string_eq(ori, cpy);
+	ori = strnstr("", "llo", 4);
+	cpy = ft_strnstr("", "llo", 4);
+	mu_assert_string_eq(ori, cpy);
+}
+
+MU_TEST_SUITE(test_string)
+{
+	MU_RUN_TEST(test_strnstr);
+}
+
+int main(void)
+{
+	// MU_RUN_SUITE(test_ctype);
+	MU_RUN_SUITE(test_string);
+	MU_REPORT();
+	return MU_EXIT_CODE;
 }

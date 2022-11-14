f35e2c141f777c3957b80d0db012168cffa72b1c
Oliems <therealoliems@gmail.com>
2022-11-14 15:07:58 +0100

    Initial commit ft_split.

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,21 @@
 /*   By: mbarberi <mbarberi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 14:00:58 by mbarberi          #+#    #+#             */
-/*   Updated: 2022/11/07 14:00:59 by mbarberi         ###   ########.fr       */
+/*   Updated: 2022/11/14 12:29:44 by mbarberi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+/*
+** Alloue (avec malloc(3)) et retourne un tableau de chaînes de caractères
+** obtenu en séparant ’s’ à l’aide du caractère ’c’, utilisé comme délimiteur.
+** Le tableau doit être terminé par NULL.
+*/
+char **ft_split(char const *s, char c)
+{
+char **p = NULL;
+(void)s;
+(void)c;
+return (p);
+}

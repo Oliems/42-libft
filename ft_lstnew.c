e9080d92121d5edc1011fe1ab690602af8bdc505
Oliems <therealoliems@gmail.com>
2022-11-15 11:16:23 +0100

    Implemented lstnew

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,22 @@
 /*   By: mbarberi <mbarberi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 19:06:49 by mbarberi          #+#    #+#             */
-/*   Updated: 2022/11/14 19:06:49 by mbarberi         ###   ########.fr       */
+/*   Updated: 2022/11/15 11:16:07 by mbarberi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+/*
+** Alloue (avec malloc(3)) et renvoie un nouvel élément. La variable membre
+** ’content’ est initialisée à l’aide de la valeur du paramètre ’content’. La
+** variable ’next’ est initialisée à NULL
+*/
+t_list *ft_lstnew(void *content)
+{
+	t_list *p;
+	p = malloc(sizeof(t_list));
+	p->content = content;
+	p->next = NULL;
+	return (p);
+}

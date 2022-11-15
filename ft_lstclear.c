2265225850898cff5be0ad2047255d0f77d0731c
Oliems <therealoliems@gmail.com>
2022-11-15 13:58:45 +0100

    Implemented lstclear and lstdelone

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,32 @@
 /*   By: mbarberi <mbarberi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 19:06:35 by mbarberi          #+#    #+#             */
-/*   Updated: 2022/11/14 19:06:36 by mbarberi         ###   ########.fr       */
+/*   Updated: 2022/11/15 13:58:10 by mbarberi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+/*
+** Supprime et libère la mémoire de l’élément passé en paramètre, et de tous
+** les éléments qui suivent, à l’aide de ’del’ et de free(3). Enfin, le
+** pointeur initial doit être mis à NULL.
+*/
+void ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*tmp;
+	t_list	*head;
+
+	head = *lst;
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		(del)((void *) (tmp->content));
+		tmp->content = NULL;
+		tmp->next = NULL;
+		free(tmp);
+		tmp = NULL;
+	}
+	*lst = NULL;
+}
--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,21 @@
 /*   By: mbarberi <mbarberi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 19:06:39 by mbarberi          #+#    #+#             */
-/*   Updated: 2022/11/14 19:06:40 by mbarberi         ###   ########.fr       */
+/*   Updated: 2022/11/15 13:58:22 by mbarberi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+/*
+** Libère la mémoire de l’élément passé en argument en utilisant la fonction
+** ’del’ puis avec free(3). La mémoire de ’next’ ne doit pas être free.
+*/
+void		ft_lstdelone(t_list *lst, void (*del)(void*))
+{
+	if (lst)
+	{
+		(del)((void *) (lst->content));
+		free(lst);
+	}
+}

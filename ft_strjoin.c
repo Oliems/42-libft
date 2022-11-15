043a63f4fc552e74a99b1ca41df2cb5f5076854a
Oliems <therealoliems@gmail.com>
2022-11-15 16:41:38 +0100

    Added protection

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,15 +6,15 @@
 /*   By: mbarberi <mbarberi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 14:01:15 by mbarberi          #+#    #+#             */
-/*   Updated: 2022/11/14 10:58:24 by mbarberi         ###   ########.fr       */
+/*   Updated: 2022/11/15 16:40:53 by mbarberi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-** Alloue (avec malloc(3)) et retourne une nouvelle
-** chaîne, résultat de la concaténation de s1 et s2.
+** Alloue (avec malloc(3)) et retourne une nouvelle chaîne, résultat de la
+** concaténation de s1 et s2.
 */
 char	*ft_strjoin(char const *s1, char const *s2)
 {
@@ -22,6 +22,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		l2;
 	char	*p;
 
+	if (!s1 || !s2)
+		return (NULL);
 	l1 = ft_strlen(s1);
 	l2 = ft_strlen(s2);
 	p = malloc(l1 + l2 + 1);

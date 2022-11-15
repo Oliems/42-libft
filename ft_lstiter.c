c67a4dca6c705bb32bd4847650d86c11ff57ccb1
Oliems <therealoliems@gmail.com>
2022-11-15 14:04:38 +0100

    Implemented lstiter

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,7 +6,25 @@
 /*   By: mbarberi <mbarberi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 19:06:42 by mbarberi          #+#    #+#             */
-/*   Updated: 2022/11/14 19:06:42 by mbarberi         ###   ########.fr       */
+/*   Updated: 2022/11/15 14:03:32 by mbarberi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+** Itère sur la liste ’lst’ et applique la fonction ’f’ au contenu chaque
+** élément.
+*/
+void		ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	t_list *p;
+
+	p = lst;
+	while (p)
+	{
+		(f)((void *)p->content);
+		p = p->next;
+	}
+}
 

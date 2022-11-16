d2389237dea2890acc5ea730cd1b4b455bdfb6f4
Oliems <therealoliems@gmail.com>
2022-11-16 18:09:29 +0100

    Test for atoi()

--- minunit/temp.c
+++ minunit/temp.c
@@ -6,150 +6,67 @@
 /*   By: mbarberi <mbarberi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/28 12:07:32 by jtoty             #+#    #+#             */
-/*   Updated: 2022/11/16 15:06:04 by mbarberi         ###   ########.fr       */
+/*   Updated: 2022/11/16 17:53:11 by mbarberi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include "../libft.h"
 
-void ft_print_result(int n)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbarberi <mbarberi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/07 13:53:46 by mbarberi          #+#    #+#             */
+/*   Updated: 2022/11/16 16:41:07 by mbarberi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+#include <stdio.h>
+
+/*
+** The atoi(3) function converts the initial portion of the nptring pointed to
+** by nptr to integer representation.
+*/
+int	ft_isdigit(int c)
 {
-	if (n > 0)
-		write(1, "1", 1);
-	else if (n < 0)
-		write(1, "-1", 2);
-	else
-		write(1, "0", 1);
+	return (c >= '0' && c <= '9');
 }
 
-/* int	ft_strncmp(const char *s1, const char *s2, size_t len)
+static int	ft_isspace(int c)
 {
-	const char	*p1;
-	const char	*p2;
+	return (c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f'
+		|| c == '\r');
+}
 
-	if (!len)
-		return (0);
-	p1 = s1;
-	p2 = s2;
-	while (len-- && (*p1 == *p2) && *p1++ && *p2++)
-		;
-	if (*(unsigned char *)(p1) - *(unsigned char *)(p2) > 0 && len)
-		return (1);
-	else if (*(unsigned char *)(p1) - *(unsigned char *)(p2) < 0 && len)
-		return (-1);
-	else
-		return (*(unsigned char *)(p1 - 1) - *(unsigned char *)(p2 - 1));
-} */
-int	ft_strncmp(const char *s1, const char *s2, size_t len)
+int	ft_atoi(const char *nptr)
 {
-	const char	*p1;
-	const char	*p2;
+	long		x;
+	int			sign;
+	const char	*p;
 
-	if (!len)
-		return (0);
-	p1 = s1;
-	p2 = s2;
-	while (1)
+	x = 0;
+	sign = 1;
+	p = nptr;
+	while (ft_isspace(*p))
+		p++;
+	if (*p == '-')
+		sign *= -1;
+	if (*p == '-' || *p == '+')
+		p++;
+	while (ft_isdigit(*p))
 	{
-		if (*p1 != *p2++)
-			return (*(unsigned char *)p1 - *(unsigned char *)--p2);
-		if (!*p1++)
-			break ;
-		if (!--len)
-			break ;
+		x *= 10;
+		x += *p - '0';
+		p++;
 	}
-	return (0);
+	return ((int)(x * sign));
 }
 
 int main(void)
 {
-	int arg = 0;
-
-	while (arg <= 11)
-	{
-		if (arg == 8)
-		{
-			ft_print_result(ft_strncmp("abcdefgh", "abcdwxyz", 4));
-			ft_print_result(strncmp("abcdefgh", "abcdwxyz", 4));
-			write(1, "\n", 1);
-		}
-
-		arg++;
-	}
-	return (0);
+	printf("atoi = %d\n", 		atoi("-11111111111111111111"));
+	printf("ft_atoi = %d\n", ft_atoi("-11111111111111111111"));
 }
-
-/* 	 if (arg == 1)
-		{
-			ft_print_result(ft_strncmp("salut", "salut", 5));
-			ft_print_result(strncmp("salut", "salut", 5));
-			write(1, "\n", 1);
-		}
-		 */
-/* 	 if (arg == 4)
-		{
-			ft_print_result(ft_strncmp("test", "tEst", 4));
-			ft_print_result(strncmp("test", "tEst", 4));
-			write(1, "\n", 1);
-		}
-
-	 if (arg == 5)
-		{
-			ft_print_result(ft_strncmp("", "test", 4));
-			ft_print_result(strncmp("", "test", 4));
-			write(1, "\n", 1);
-		}
-
-	 if (arg == 6)
-		{
-			ft_print_result(ft_strncmp("test", "", 4));
-			ft_print_result(strncmp("test", "", 4));
-			write(1, "\n", 1);
-		}
-
-	 if (arg == 7)
-		{
-			ft_print_result(ft_strncmp("abcdefghij", "abcdefgxyz", 3));
-			ft_print_result(strncmp("abcdefghij", "abcdefgxyz", 3));
-			write(1, "\n", 1);
-		}
-
-
-
-	 if (arg == 9)
-		{
-			ft_print_result(ft_strncmp("zyxbcdefgh", "abcdwxyz", 0));
-			ft_print_result(strncmp("zyxbcdefgh", "abcdwxyz", 0));
-			write(1, "\n", 1);
-		}
-
-	 if (arg == 10)
-		{
-			ft_print_result(ft_strncmp("abcdefgh", "", 0));
-			ft_print_result(strncmp("abcdefgh", "", 0));
-			write(1, "\n", 1);
-		} */
-
-/* 	 if (arg == 2)
-		{
-			ft_print_result(ft_strncmp("test", "testss", 7));
-			ft_print_result(strncmp("test", "testss", 7));
-			write(1, "\n", 1);
-		}
-
-	 if (arg == 3)
-		{
-			ft_print_result(ft_strncmp("testss", "test", 7));
-			ft_print_result(strncmp("testss", "test", 7));
-			write(1, "\n", 1);
-		}
-
-	 if (arg == 11)
-		{
-			ft_print_result(ft_strncmp("test\200", "test\0", 6));
-			ft_print_result(strncmp("test\200", "test\0", 6));
-			write(1, "\n", 1);
-		} */

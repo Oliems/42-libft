63b232d4e6ce4453eecae7cb9c4c371a5552fbff
Oliems <therealoliems@gmail.com>
2022-11-16 12:56:41 +0100

    Bug fixing and optimization

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mbarberi <mbarberi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 14:01:32 by mbarberi          #+#    #+#             */
-/*   Updated: 2022/11/11 17:21:03 by mbarberi         ###   ########.fr       */
+/*   Updated: 2022/11/16 12:56:13 by mbarberi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,17 @@
 */
 int	ft_strncmp(const char *s1, const char *s2, size_t len)
 {
-	const char	*p1;
-	const char	*p2;
+	unsigned char	*p1;
+	unsigned char	*p2;
 
-	p1 = s1;
-	p2 = s2;
-	while (len--)
-	{
-		if (*p1++ != *p2++)
-		{
-			if ((*(unsigned char *)(p1 - 1) - *(unsigned char *)(p2 - 1)) > 0)
-				return (1);
-			else
-				return (-1);
-		}
-	}
-	return (0);
+	p1 = (unsigned char *)s1;
+	p2 = (unsigned char *)s2;
+	while (len-- && (*p1++ == *p2++) && *p1 && *p2)
+		;
+	if (*(p1 - 1) - *(p2 - 1) > 0)
+		return (1);
+	else if (*(p1 - 1) - *(p2 - 1)  < 0)
+		return (-1);
+	else
+		return (0);
 }
